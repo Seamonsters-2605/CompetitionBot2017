9b35b2c9f89c3728051e7223e664df6d1ce93aca
Jacob van't Hoog <vantjac@gmail.com>
2017-01-08 10:52:44 -0800

    Add drive bot

---
+++ drive.py
@@ -0,0 +1,96 @@
+__author__ = "seamonsters"
+
+import wpilib
+from seamonsters.wpilib_sim import simulate
+
+from seamonsters.drive import DriveInterface
+from seamonsters.gamepad import Gamepad
+from seamonsters.drive import DriveInterface
+from seamonsters.drive import AccelerationFilterDrive
+from seamonsters.drive import FieldOrientedDrive
+from seamonsters.holonomicDrive import HolonomicDrive
+
+from robotpy_ext.common_drivers.navx import AHRS
+import math
+
+class DriveBot(wpilib.IterativeRobot):
+
+    def robotInit(self):
+        self.gamepad = Gamepad(port = 0)
+        
+        fl = wpilib.CANTalon(2)
+        fr = wpilib.CANTalon(1)
+        bl = wpilib.CANTalon(0)
+        br = wpilib.CANTalon(3)
+        self.talons = [fl, fr, bl, br]
+        
+        for talon in self.talons:
+            talon.setFeedbackDevice(wpilib.CANTalon.FeedbackDevice.QuadEncoder)
+        
+        self.currentPID = None
+        self.normalPID = (10.0, 0.0, 3.0, 0.0)
+        self.slowPID = (30.0, 0.0, 3.0, 0.0)
+        self._setPID(self.normalPID)
+        
+        # 4156 ticks per wheel rotation
+        # encoder has 100 raw ticks -- with a QuadEncoder that makes 400 ticks
+        # the motor gear has 18 teeth and the wheel has 187 teeth
+        # 187 / 18 * 400 = 4155.5556 = ~4156
+        # TODO: recalculate ticks per rotation
+        self.drive = HolonomicDrive(fl, fr, bl, br, 4156)
+        self.drive.invertDrive(True)
+        self.drive.setWheelOffset(math.radians(22.5)) #angle of rollers
+        
+        self.drive = AccelerationFilterDrive(self.drive)
+        
+        self.ahrs = AHRS.create_spi() # the NavX
+        self.drive = FieldOrientedDrive(self.drive, self.ahrs)
+        
+        self.drive.setDriveMode(DriveInterface.DriveMode.POSITION)
+        
+        self.normalScale = 0.3
+        self.fastScale = 0.5
+        self.slowScale = 0.05
+        
+    def teleopInit(self):
+        print("Left Bumper: Slower")
+        print("Left Trigger: Faster")
+        
+    def teleopPeriodic(self):
+        # change drive mode with A, B, and X
+        if   self.gamepad.getRawButton(Gamepad.A):
+            print("Voltage mode!")
+            self.drive.setDriveMode(DriveInterface.DriveMode.VOLTAGE)
+        elif self.gamepad.getRawButton(Gamepad.B):
+            print("Speed mode!")
+            self.drive.setDriveMode(DriveInterface.DriveMode.SPEED)
+        elif self.gamepad.getRawButton(Gamepad.X):
+            print("Position mode!")
+            self.drive.setDriveMode(DriveInterface.DriveMode.POSITION)
+        
+        scale = self.normalScale
+        if self.gamepad.getRawButton(Gamepad.LT): # faster button
+            scale = self.fastScale
+        if self.gamepad.getRawButton(Gamepad.LB): # slower button
+            scale = self.slowScale
+            self._setPID(self.slowPID)
+        else:
+            self._setPID(self.normalPID)
+        
+        turn = -self.gamepad.getRX() * abs(self.gamepad.getRX()) \
+            * (scale / 2)
+        magnitude = self.gamepad.getLMagnitude()**2 * scale
+        direction = self.gamepad.getLDirection()
+        
+        self.drive.drive(magnitude, direction, turn)
+        
+    def _setPID(self, pid):
+        if pid == self.currentPID:
+            return
+        self.currentPID = pid
+        for talon in self.talons:
+            talon.setPID(pid[0], pid[1], pid[2], pid[3])
+
+if __name__ == "__main__":
+    wpilib.run(DriveBot)
+
--- robot.py
+++ robot.py
@@ -4,10 +4,13 @@ import wpilib
 from seamonsters.wpilib_sim import simulate
 from seamonsters.modularRobot import Module
 
+from drive import DriveBot
+
 class CompetitionBot2017(Module):
     
     def __init__(self):
         super().__init__()
+        self.addModule(DriveBot())
         
 if __name__ == "__main__":
     wpilib.run(CompetitionBot2017)
