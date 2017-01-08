3f4a9738498ecd27d33354381cb8366b66c4d798
Jacob van't Hoog <vantjac@gmail.com>
2017-01-08 10:58:36 -0800

    Don't init super() for modules

--- robot.py
+++ robot.py
@@ -10,7 +10,7 @@ class CompetitionBot2017(Module):
     
     def __init__(self):
         super().__init__()
-        self.addModule(DriveBot())
+        self.addModule(DriveBot(initSuper=False))
         
 if __name__ == "__main__":
     wpilib.run(CompetitionBot2017)
