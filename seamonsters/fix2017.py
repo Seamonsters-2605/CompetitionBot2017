b27fcd572bdb6e03ca86e91ddd229b6a57abcb44
Jacob van't Hoog <vantjac@gmail.com>
2017-01-11 19:58:35 -0800

    Temporary fix for tests

--- seamonsters/fix2017.py
+++ seamonsters/fix2017.py
@@ -1,4 +1,24 @@
 __author__ = "jacobvanthoog"
 
-print("Fix 2017!")
+import wpilib
 
+wpilib.CANTalon = wpilib.TalonSRX
+
+def setFeedbackDevice(self, device):
+    pass
+wpilib.CANTalon.setFeedbackDevice = setFeedbackDevice
+
+class FeedbackDevice:
+    QuadEncoder = 0
+    AnalogPot = 1
+    AnalogEncoder = 2
+    EncRising = 3
+    EncFalling = 4
+    CtreMagEncoder_Relative = 5
+    CtreMagEncoder_Absolute = 6
+    PulseWidth = 7
+wpilib.CANTalon.FeedbackDevice = FeedbackDevice
+
+def setPID(self, p, i, d, f):
+    pass
+wpilib.CANTalon.setPID = setPID
