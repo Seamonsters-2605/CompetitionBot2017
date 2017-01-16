2d695d5b083bedbc2dc4b7748d8d8392c5b0a4d2
Jacob van't Hoog <vantjac@gmail.com>
2017-01-16 11:07:36 -0800

    Zero navX when robot code starts

--- drive.py
+++ drive.py
@@ -52,7 +52,8 @@ class DriveBot(Module):
         self.drive = AccelerationFilterDrive(self.drive)
         
         self.ahrs = AHRS.create_spi() # the NavX
-        self.drive = FieldOrientedDrive(self.drive, self.ahrs)
+        self.drive = FieldOrientedDrive(self.drive, self.ahrs, offset=math.pi/2)
+        self.drive.zero()
         
         self.drive.setDriveMode(DriveInterface.DriveMode.POSITION)
         
