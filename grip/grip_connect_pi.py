64319db1d3b263134f1e66e41b3aed131a2d5d43
Jacob van't Hoog <vantjac@gmail.com>
2017-01-24 20:13:50 -0800

    Remove pi test script

--- grip/grip_connect_pi.py
+++
@@ -1,9 +0,0 @@
-from networktables import NetworkTables
-
-NetworkTables.initialize(server="roborio-2605-frc.local")
-
-testTable = NetworkTables.getTable('test-table')
-
-while True:
-    testTable.putNumber('testNumber', 1235)
-    print(testTable.getNumber('testNumber'))
