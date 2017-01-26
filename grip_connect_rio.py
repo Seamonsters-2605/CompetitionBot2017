cabeed4c7e79ffa411193c480420d260a27e3693
Jacob van't Hoog <vantjac@gmail.com>
2017-01-25 19:31:45 -0800

    Read contour data from NetworkTables

--- grip_connect_rio.py
+++ grip_connect_rio.py
@@ -8,8 +8,43 @@ class NetworkTablesTestRobot(wpilib.IterativeRobot):
         self.contoursTable = NetworkTables.getTable('contours')
 
     def teleopPeriodic(self):
-        print(self.contoursTable.getNumberArray('x'),
-              self.contoursTable.getNumberArray('y'))
+        contours = self.readContours(self.contoursTable.getNumberArray('x'),
+                                     self.contoursTable.getNumberArray('y'))
+        print(contours)
+
+    def readContours(self, xCoords, yCoords):
+        """
+        Read contours from the data sent over NetworkTables.
+
+        A coordinate is a tuple of 2 values. A contour is a list of coordinates.
+        This function returns a list of contours - so a list of lists of tuples.
+        """
+        # check data
+        if len(xCoords) != len(yCoords):
+            print("ERROR: Incorrect contour data! "
+                  "len(xCoords) != len(yCoords)")
+            return [ ]
+        numContours = xCoords.count(-1)
+        if numContours != yCoords.count(-1):
+            print("ERROR: Incorrect contour data! "
+                  "xCoords.count(-1) != yCoords.count(-1)")
+            return [ ]
+
+        contours = [ ]
+        currentContour = [ ]
+        for i in range(0, len(xCoords)):
+            x = xCoords[i]
+            y = yCoords[i]
+            if x == -1:
+                if len(currentContour) != 0:
+                    contours.append(currentContour)
+                currentContour = [ ]
+            else:
+                currentContour.append( (x, y) )
+        if len(currentContour) != 0:
+            contours.append(currentContour)
+
+        return contours
 
 if __name__ == "__main__":
     wpilib.run(NetworkTablesTestRobot)
