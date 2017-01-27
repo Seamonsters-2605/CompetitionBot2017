ef1c428e2ba2b671235861cf6c7e605d7d0ee4d6
Jacob van't Hoog <vantjac@gmail.com>
2017-01-26 19:36:09 -0800

    Update comment

--- drive.py
+++ drive.py
@@ -69,11 +69,10 @@ class DriveBot(Module):
         self._setPID(self.fastPID)
         self.driveScales = [0.0 for i in range(0, pidLookBackRange)]
         
-        # 4156 ticks per wheel rotation
+        # 2833 ticks per wheel rotation
         # encoder has 100 raw ticks -- with a QuadEncoder that makes 400 ticks
         # the motor gear has 12 teeth and the wheel has 85 teeth
         # 85 / 12 * 400 = 2833.333 = ~2833
-        # TODO: test this value
         self.holoDrive = HolonomicDrive(fl, fr, bl, br, 2833)
         self.holoDrive.invertDrive(True)
         self.holoDrive.setWheelOffset(math.radians(22.5)) #angle of rollers
