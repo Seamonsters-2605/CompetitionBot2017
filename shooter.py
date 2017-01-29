05c6091940499d2b3ffa012e82755b700b4a895a
Jacob van't Hoog <vantjac@gmail.com>
2017-01-29 13:52:50 -0800

    Allow adjusting flywheel speed

--- shooter.py
+++ shooter.py
@@ -5,6 +5,7 @@ import seamonsters.fix2017
 from seamonsters.wpilib_sim import simulate
 from seamonsters.modularRobot import Module
 from seamonsters.gamepad import Gamepad
+from seamonsters.logging import LogState
 
 class Shooter (Module):
     
@@ -14,15 +15,26 @@ class Shooter (Module):
         self.sh = wpilib.CANTalon(5)
         self.it = wpilib.CANTalon(6)
 
+        self.flywheelSpeedLog = LogState("Flywheel speed")
+
     def teleopInit(self):
         print("A is Flywheel")
         print("B is Intake")
         print("X is Outtake")
 
+        self.flywheelSpeed = .76
+        self.flywheelSpeedLog.update(self.flywheelSpeed)
+
     def teleopPeriodic(self):
 
+        if self.gamepad.buttonPressed(Gamepad.BACK):
+            self.flywheelSpeed -= .01
+        if self.gamepad.buttonPressed(Gamepad.START):
+            self.flywheelSpeed += .01
+        self.flywheelSpeedLog.update(self.flywheelSpeed)
+
         if self.gamepad.getRawButton(Gamepad.A):
-            self.sh.set(-0.9)
+            self.sh.set(-self.flywheelSpeed)
         else:
             self.sh.set(0)
 
@@ -32,6 +44,8 @@ class Shooter (Module):
             self.it.set(-0.2)
         else:
             self.it.set(0)
+
+        self.gamepad.updateButtons()
 
 if __name__ == "__main__":
     wpilib.run(Shooter)
