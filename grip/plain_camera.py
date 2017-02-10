a3f825325c2fdec62723bb3708e3e2222e982c08
seamonsters2605 <frc2605@gmail.com>
2017-02-10 20:00:28 +0000

    IT WORKSgit add .!

--- grip/plain_camera.py
+++ grip/plain_camera.py
@@ -6,8 +6,11 @@ def main():
     cam = cv2.VideoCapture(0) # use the 2nd camera
     while True:
         ret, frame = cam.read()
-        frame = cv2.cvtColor(frame, cv2.COLORBGR2GRAY)
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow('camera2', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+             break
+    cam.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
