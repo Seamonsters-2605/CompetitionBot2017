6b77f6cb7b974534fe10461b96adf8e48b6872b0
Tadeusz Pforte <tadtiger13@gmail.com>
2017-02-11 09:36:46 -0800

    Added missing backslashes

--- createstartupscript.sh
+++ createstartupscript.sh
@@ -3,8 +3,8 @@
 echo "#!/bin/sh -e" > /etc/rc.local
 echo "# rc.local\n" >> /etc/rc.local
 echo "_IP=$(hostname -I) || true" >> /etc/rc.local
-echo "if [ \"$_IP\" ]; then" >> /etc/rc.local
-echo "  printf \"My IP address is %s\\n\" \"$_IP\"" >> /etc/rc.local
+echo "if [ \"\$_IP\" ]; then" >> /etc/rc.local
+echo "  printf \"My IP address is %s\n\" \"\$_IP\"" >> /etc/rc.local
 echo "fi" >> /etc/rc.local
 echo "sudo ifdown wlan0" >> /etc/rc.local
 echo "sudo ifup eth0\n" >> /etc/rc.local
