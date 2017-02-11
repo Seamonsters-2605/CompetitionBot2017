f9303559a284af75d859a27d265fb4931ba0e805
Tadeusz Pforte <tadtiger13@gmail.com>
2017-02-11 10:32:24 -0800

    Added debug logging

--- createstartupscript.sh
+++ createstartupscript.sh
@@ -2,6 +2,8 @@
 
 echo "#!/bin/sh -e" > /etc/rc.local
 echo "# rc.local\n" >> /etc/rc.local
+echo "exec 2> /var/log/seamonsters.log" >> /etc/rc.local
+echo "set -x\n" >> /etc/rc.local
 echo "sudo ifdown wlan0" >> /etc/rc.local
 echo "sudo ifup eth0\n" >> /etc/rc.local
 echo "python3 /home/pi/seamonsters_git/CompetitionBot2017/grip/pi_script.py &" >> /etc/rc.local
