7535f02ad230b7825422f400d341b3149d799b7f
Tadeusz Pforte <tadtiger13@gmail.com>
2017-02-11 09:51:19 -0800

    Changed both to pi_script.py from filter_test_contours.py

--- createstartupscript.sh
+++ createstartupscript.sh
@@ -4,6 +4,6 @@ echo "#!/bin/sh -e" > /etc/rc.local
 echo "# rc.local\n" >> /etc/rc.local
 echo "sudo ifdown wlan0" >> /etc/rc.local
 echo "sudo ifup eth0\n" >> /etc/rc.local
-echo "python3 /home/pi/seamonsters_git/CompetitionBot2017/grip/filter_test_contours.py peg_filter_real &" >> /etc/rc.local
+echo "python3 /home/pi/seamonsters_git/CompetitionBot2017/grip/pi_script.py &" >> /etc/rc.local
 echo "python3 /home/pi/seamonsters_git/CompetitionBot2017/grip/plain_camera.py &" >> /etc/rc.local
 echo "exit 0" >> /etc/rc.local
