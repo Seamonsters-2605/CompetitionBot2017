61959cad28d4a498ea66aadb8119fb0a74040333
Jacob van't Hoog <vantjac@gmail.com>
2017-02-18 17:36:43 -0800

    Clean up gear test; use test mode

--- gear_test.py
+++ gear_test.py
@@ -9,33 +9,9 @@ from seamonsters.modularRobot import Module
 class GearTest(Module):
     def robotInit(self):
         self.proximitySensor = wpilib.AnalogInput(0)
-        # when the robot code starts (only once, not each time it's enabled)
-        # Declare Gamepads and CANTalons here.
-        # Don't access encoder values here; do that in teleopInit or
-        # autonomousInit.
-        pass
 
-    def teleopInit(self):
-        # when teleop mode starts
-        pass
-
-    def teleopPeriodic(self):
+    def testPeriodic(self):
         print(self.proximitySensor.getVoltage())
-        # 50 times per second in teleop mode
-        pass
-
-    def autonomousInit(self):
-        # when autonomous mode starts
-        pass
-
-    def autonomousPeriodic(self):
-        # 50 times per second in autonomous mode
-        pass
-
-    def disabledInit(self):
-        # when the robot is disabled
-        pass
-
 
 if __name__ == "__main__":
     wpilib.run(GearTest)
