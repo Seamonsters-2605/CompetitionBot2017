9a95c49a8b2ef26d670a929367b26fb27955ac8e
Seamonsters <frc2605@gmail.com>
2017-03-19 14:15:58 -0700

    Add a second camera

--- cscore_camera.py
+++ cscore_camera.py
@@ -1,10 +1,15 @@
 try:
     import cscore
+
     camera = cscore.UsbCamera("usbcam", 0)
     camera.setVideoMode(cscore.VideoMode.PixelFormat.kMJPEG, 320, 240, 30)
-
     mjpegServer = cscore.MjpegServer("httpserver", 1187)
     mjpegServer.setSource(camera)
+
+    otherCamera = cscore.UsbCamera("usbcam-boiler", 1)
+    otherCamera.setVideoMode(cscore.VideoMode.PixelFormat.kMJPEG, 320, 240, 30)
+    otherMjpegServer = cscore.MjpegServer("httpserver-boiler", 1188)
+    otherMjpegServer.setSource(otherCamera)
 except BaseException as e:
     print("cscore error")
     print(e)
