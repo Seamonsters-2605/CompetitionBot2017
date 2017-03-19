96bd4c76cf7dbaad619ba4eed710978fae2f5bd3
Seamonsters <frc2605@gmail.com>
2017-03-18 22:22:38 -0700

    Change camera port

--- cscore_camera.py
+++ cscore_camera.py
@@ -3,7 +3,7 @@ try:
     camera = cscore.UsbCamera("usbcam", 0)
     camera.setVideoMode(cscore.VideoMode.PixelFormat.kMJPEG, 320, 240, 30)
 
-    mjpegServer = cscore.MjpegServer("httpserver", 8081)
+    mjpegServer = cscore.MjpegServer("httpserver", 1187)
     mjpegServer.setSource(camera)
 except BaseException as e:
     print("cscore error")
